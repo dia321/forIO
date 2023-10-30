73947d1083f54f171925b2e36ac36caf1e77288d
김성민 <seongmin.kim@kbfg.com>
2023-10-30 13:34:34 +0900

    setting refresh

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Chart from './pages/Chart/components/Chart';
 
 const Text = lazy(() => import('./pages/Text'));
 const Dog = lazy(() => import('./pages/Dog/components/Dog'));
@@ -11,7 +10,6 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/text" element={<Text />} />
         <Route path="/dog" element={<Dog />} />
-        <Route path="/chart" element={<Chart />} />
       </Routes>
     </Suspense>
   );
