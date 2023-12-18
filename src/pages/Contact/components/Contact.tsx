56171c9dcbb3a447d841766e875d725347ee4b2c
김성민 <seongmin.kim@kbfg.com>
2023-12-18 18:00:22 +0900

    slide animation

--- src/pages/Contact/components/Contact.tsx
+++ src/pages/Contact/components/Contact.tsx
@@ -5,17 +5,22 @@ import exPhoto2 from '@assets/skill-image.png';
 import exPhoto3 from '@assets/profile-image.png';
 import AngleLeftIcon from '@assets/angle-left-icon.svg?react';
 import AngleRightIcon from '@assets/angle-right-icon.svg?react';
-import { Fragment, useRef, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
 const Contact = () => {
-  const [photoIndex, setPhotoIndex] = useState(0);
   const photoRef = useRef<HTMLDivElement>(null);
   const photoList = [smPhoto, exPhoto1, exPhoto2, exPhoto3];
-  const handleSlide = (idx: number) => {
-    if (photoRef.current) {
-      photoRef.current.style['marginLeft'] = `${idx * -224}px`;
-    }
-  };
+  const [slide, setSlide] = useState({
+    ing: false,
+    direction: '',
+    idx: 0
+  });
+  useEffect(() => {
+    if (slide.ing)
+      setTimeout(() => {
+        setSlide({ ...slide, ing: false });
+      }, 500);
+  }, [slide.ing]);
   return (
     <div className={s['contact']}>
       <div className={s['info-card']}>
@@ -23,16 +28,20 @@ const Contact = () => {
         <div className={s['card-inner']}>
           <div
             className={`${s['angle-zone']} ${s['left']}`}
-            onClick={() => {
-              const nextIdx = photoIndex <= 0 ? 3 : photoIndex - 1;
-              handleSlide(nextIdx);
-              setPhotoIndex(nextIdx);
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log('asdfasdf', slide.ing);
+              if (slide.ing) return;
+              setSlide({ idx: slide.idx === 0 ? 3 : slide.idx - 1, direction: 'left', ing: true });
             }}
           >
             <AngleLeftIcon />
           </div>
           <div className={s['photo-zone']}>
-            <div className={s['ref-control']} ref={photoRef}>
+            <div
+              className={`${s['ref-control']} ${s[`i-${slide.idx}`]} ${s[`${slide.direction}`]}`}
+              ref={photoRef}
+            >
               {photoList.map((p, idx) => (
                 <Fragment key={`photo${idx}`}>
                   <img src={p} className={s['photo-container']} />
@@ -42,10 +51,10 @@ const Contact = () => {
           </div>
           <div
             className={`${s['angle-zone']} ${s['right']}`}
-            onClick={() => {
-              const nextIdx = photoIndex <= 0 ? 3 : photoIndex - 1;
-              handleSlide(nextIdx);
-              setPhotoIndex(nextIdx);
+            onClick={(e) => {
+              e.stopPropagation();
+              if (slide.ing) return;
+              setSlide({ idx: slide.idx === 3 ? 0 : slide.idx + 1, direction: 'right', ing: true });
             }}
           >
             <AngleRightIcon />
